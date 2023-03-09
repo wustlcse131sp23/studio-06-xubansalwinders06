11fd4290de3145ac1b343410b9b94d3fe5602cce
gurksb <gurksb@localhost>
2023-03-09 16:53:27 -0600

    done!!!!

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -12,10 +12,13 @@ public class RecursiveMethods {
 	 *         ...)
 	 */
 	public static double geometricSum(int n) {
-		
-			// FIXME compute the geometric sum for the first n terms recursively
+		// FIXME compute the geometric sum for the first n terms recursively
+		if (n==0) {
 			return 0;
-		
+		} 
+		else {
+			return 1/(Math.pow(2, (n))) + geometricSum(n-1);
+		}
 	}
 
 	/**
@@ -27,13 +30,18 @@ public class RecursiveMethods {
 	 * @return greatest common divisor of p and q
 	 */
 	public static int gcd(int p, int q) {
-		
-			// FIXME compute the gcd of p and q using recursion
-			return 0;
-		
+
+		if (p % q == 0) {
+			return q;
+		}
+		else {
+			return gcd(q, p % q);
+		}
+		// FIXME compute the gcd of p and q using recursion
+
 	}
 
-	
+
 
 	/**
 	 * This method uses recursion to create a reverse of the given array
@@ -41,11 +49,31 @@ public class RecursiveMethods {
 	 * @param array the array to create a reverse of, not to be mutated
 	 * @return an array with the same data as the input but it reverse order
 	 */
+	public static int[] toReverseHelper (int [] emptyArray, int[] array, int counter) {
+		if (counter > array.length - 1) {
+			return emptyArray;
+		}
+	else {
+		int index = array.length - counter;
+		emptyArray[counter] = array [index - 1];
+		counter ++;
+		return toReverseHelper(emptyArray, array, counter);
+		}
+
+	}
+
 	public static int[] toReversed(int[] array) {
-		
-			// FIXME create a helper method that can recursively reverse the given array
-			return new int[0];
-		
+		if (array.length != 0) {
+			int [] emptyArray = new int[array.length];
+			int counter = 0;
+			return toReverseHelper (emptyArray, array, counter);
+		} else {
+			return new int [0];
+		}
+
+		// FIXME create a helper method that can recursively reverse the given array
+	
+
 	}
 
 	/**
@@ -59,7 +87,18 @@ public class RecursiveMethods {
 	 */
 	public static void circlesUponCircles(double xCenter, double yCenter, double radius,
 			double radiusMinimumDrawingThreshold) {
-		
+//if (radius <= radiusMinimumDrawingThreshold) {
+//	
+//} else {
+//
+//	return circlesUponCircles(xCenter, yCenter, radius, radiusMinimumDrawingThreshold);
+//	return circlesUponCircles(xCenter + radius, yCenter, radius/3.0, radiusMinimumDrawingThreshold);
+//	return circlesUponCircles(xCenter, yCenter + radius , radius/3.0, radiusMinimumDrawingThreshold);
+//	return circlesUponCircles(xCenter, yCenter - radius , radius/3.0, radiusMinimumDrawingThreshold);
+//	return circlesUponCircles(xCenter - radius, yCenter, radius/3.0, radiusMinimumDrawingThreshold);
+//	
+//
+//}
 		// FIXME
 	}
 
